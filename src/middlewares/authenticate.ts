0605988f3ad155c791e61cf1f4199e4654c27cf8
Ritesh Khore <riteshkhore@gmail.com>
2023-05-13 16:20:13 +0530

    added build and start command

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -1,9 +1,10 @@
 import { NextFunction, Request, Response } from "express"
+import { IUser } from "../types"
 
 export function authenticate(req: Request, res: Response, next: NextFunction) {
   if (req.isAuthenticated()) {
     req.locals = {
-      user: req.user,
+      user: req.user as IUser,
       isAuthenticated: true,
     }
     return next()
